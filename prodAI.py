b075e6a1a0477bf3cc26245f98b4f0848bbe5af7
Shreyaanp <shreyaan.pradhan2020@vitstudeent.ac.in>
2024-05-07 21:45:10 +0545

    changes made

--- prodAI.py
+++ prodAI.py
@@ -42,8 +42,8 @@ web_fetch_tool = Tool.from_function(
     description="Fetches the content of a web page"
 )
 
-prompt_template = "Summarize the following content in more or less than 400 words. The content is about a product and we need to generate SEO friendly data: {content}"
-llm = ChatOpenAI(model="gpt-3.5-turbo-16k")
+prompt_template = "Summarize the following content in more or less than 400 words. The content is about a product and we need to generate SEO friendly data. Also you have to search the internet for all different names the product is called by in local or regional parts of the world. scrape the internet and find all the names , it could have multiple name and then list them.: {content}"
+llm = ChatOpenAI(model="gpt-4-turbo")
 llm_chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(prompt_template))
 summarize_tool = Tool.from_function(
     func=llm_chain.run,  # Note: Adjust according to actual function reference.
@@ -64,9 +64,13 @@ agent = initialize_agent(
 prompt = None
 
 def process_prompt(prompt: str) -> str:
-    post_prompt = '''. Use your tools to search and create a proper product name, product description, about product in podints, and a product tagline. The output should be in this format :
+    post_prompt = '''. Use your tools to search the internet about the product and its aletnate names in local terms in India in english.
+      You need to search the internet and find all the different names of the product called in different regions all around India in english, along with the location of the native name using the web search, there can be more than one single name so do not miss the other names. 
+    Create a proper product name, product description, about product in points, a product tagline.
+    In product description also add the history of the product as well the the benefit of the product with the target as explanation of the product as to why this product should be used. The intension is to display as much information about the product in the most innovative way as possible. The output should be in this format :
 Product Name: [product name]
-Product Description: [product description]
+Different names of the Product: [different names of the product(along with the the local area of the native name), another product name(local area of the native name).... and so on]
+Product Description: [product description should be minimum 400 words]
 About Product: [about product in points]
 Product Tagline: [product tagline]
 also generate a SEO friendly data for the product, like tags.
@@ -80,7 +84,7 @@ Do not break the above format.
 
 if __name__ == "__main__":
     # Test processing a prompt
-    test_prompt = "Name of the product : intitle:kala namak rice"
+    test_prompt = "Name of the product : intitle : काला नमक चावल"
     print("Processing Prompt:", test_prompt)
     processed_prompt = process_prompt(test_prompt)
     print("Processed Prompt Result:", processed_prompt)
